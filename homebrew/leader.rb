acf567ec98a80b97bb1f6181b587dfbb899a3c39
Jens Norrgrann <jens.norrgrann@bukowskis.com>
2013-11-28 10:59:11 +0100

    homebrew formula

---
+++ homebrew/leader.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class Leader < GithubGistFormula
+  homepage 'https://github.com/bukowskis/leader'
+  url 'https://raw.github.com/bukowskis/leader/1e28473985316bcf900f5cfbb44c4bb115f4930e/leader'
+  sha1 '7d311a18ef585358a013c8c05617d5bb13456a6d'
+  version '1.0.3'
+end
