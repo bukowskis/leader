ea57f3d757092d1be5e5ac703c73cd1a45e3df34
mmzoo <mmzoo@posteo.is>
2014-12-15 11:19:50 +0100

    Trying to update homebrew formula

--- homebrew/leader.rb
+++ homebrew/leader.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Leader < GithubGistFormula
   homepage 'https://github.com/bukowskis/leader'
-  url 'https://raw.github.com/bukowskis/leader/1e28473985316bcf900f5cfbb44c4bb115f4930e/leader'
-  sha1 '7d311a18ef585358a013c8c05617d5bb13456a6d'
+  url 'https://raw.github.com/bukowskis/leader/80aabb26b881c24c0e401973986a607fa1af265f/leader'
+  sha1 'd2ab2e1a2e265cdd8d657537453fca521c466106'
   version '1.0.3'
 end
