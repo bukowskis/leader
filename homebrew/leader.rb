037a818bf4a7ea9a33e882584a82043df9102d85
mmzoo <mmzoo@posteo.is>
2014-12-15 11:30:35 +0100

    Homebrew formula bump 1.0.3 -> 1.1.0

--- homebrew/leader.rb
+++ homebrew/leader.rb
@@ -4,5 +4,5 @@ class Leader < GithubGistFormula
   homepage 'https://github.com/bukowskis/leader'
   url 'https://raw.github.com/bukowskis/leader/80aabb26b881c24c0e401973986a607fa1af265f/leader'
   sha1 'd2ab2e1a2e265cdd8d657537453fca521c466106'
-  version '1.0.3'
+  version '1.1.0'
 end
